564ca8a9738644be8878adc1b328381882da85b5
Blackflunk <Alexander12350@hotmail.com>
2014-11-18 14:23:19 +0100

    Dice og DiceBox oprettet og rettet

--- CDIO3/src/FieldGame/Dice.java
+++ CDIO3/src/FieldGame/Dice.java
@@ -1,5 +1,35 @@
 package FieldGame;
 
 public class Dice {
+	private int dice;
+	private int diceside = 6;
+	
+	public Dice(){
+		super();
+	}
+	
+	public int getDice(){
+		return dice;
+	}
+	
+	public void setDice(int dice){
+		this.dice = dice;
+	}
+	
+	public int getDiceside(){
+		return diceside;
+	}
+	
+	public void setDiceside(int new_diceside) {
+		this.diceside = new_diceside;
+	}
 
+	public int rollDice(){
+		dice = (int)(Math.random()*diceside+1);
+		return dice;
+	}
+	
+	public String toString() {
+		return ("The dice with " + diceside + " sides, has the current value of: " + dice); 
+	}
 }
---
+++ CDIO3/src/FieldGame/DiceBox.java
@@ -0,0 +1,43 @@
+package FieldGame;
+
+public class DiceBox {
+	
+	//Dice t1;
+	//Dice t2;
+	int numberOfDice = 2;
+	Dice[] box  = new Dice[numberOfDice];
+	
+	public DiceBox(){
+	this.box[0] = new Dice(); 
+	this.box[1] = new Dice(); 
+	}
+
+	public int getDice(int dicenumber) {
+		return box[dicenumber].getDice();
+	}
+	
+	public void setDice(int dicenumber, int new_dicevalue) {
+		box[dicenumber].setDice(new_dicevalue);
+	}
+	
+	public int getDiceside(int dicenumber) {
+		return box[dicenumber].getDiceside();
+	}
+	
+	public void rollDice(){
+		box[0].rollDice();
+		box[1].rollDice();
+	}
+	
+	public int getSum() {
+		return (box[0].getDice() + box[1].getDice());
+	}
+	
+	public void setDiceside(int dicenumber, int new_diceside){
+		box[dicenumber].setDiceside(new_diceside);
+	}
+
+	public String toString() {
+		return ("The dicebox has the values " + box[0].getDice() + " and " + box[1].getDice());
+	}
+}

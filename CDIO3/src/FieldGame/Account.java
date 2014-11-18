6de3627041471ece387b6bc504d8906b41d8d1ac
Blackflunk <Alexander12350@hotmail.com>
2014-11-18 11:33:34 +0100

    Account og Player opdateret

--- CDIO3/src/FieldGame/Account.java
+++ CDIO3/src/FieldGame/Account.java
@@ -1,5 +1,42 @@
 package FieldGame;
 
 public class Account {
-
+		
+	private int balance;
+	
+	public Account() {
+		this.balance = 0;
+	}
+	
+	public Account(int balance) {
+		this.balance = balance;
+	}
+	
+	public int getBalance() {
+		if (balance < 0)
+			balance = 0;
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public void addBalance(int add) {
+		if (balance < 0)
+			this.balance = add;
+		else
+			this.balance = balance + add;
+	}
+	
+	public boolean CheckWin() {
+		if (balance >= 3000)
+			return true;
+		else
+			return false;
+	}
+	
+	public String toString() {
+		return ("current balance: " + getBalance());
+	}
 }

2d83603f0d69a5e6c97c5dcabf809e0109a28c98
AoschkA <jonaspraem@gmail.com>
2014-11-19 09:34:47 +0100

    Tax klasse skrevet

--- CDIO3/src/FieldGame/Tax.java
+++ CDIO3/src/FieldGame/Tax.java
@@ -1,5 +1,27 @@
 package FieldGame;
 
 public class Tax {
+	private int taxAmmount;
+	private int taxRate = -1;
+	
+	public Tax() {
+		super();
+	}
+	public Tax(int taxAmmount) {
+		this.taxAmmount = taxAmmount;
+	}
+	public int getTaxAmmount() {
+		return taxAmmount;
+	}
+	public void setTaxAmmount(int taxAmmount) {
+		this.taxAmmount = taxAmmount;
+	}
+	public int getTaxRate() {
+		return taxRate;
+	}
+	public void setTaxRate(int taxRate) {
+		this.taxRate = taxRate;
+	}
+	
 
 }

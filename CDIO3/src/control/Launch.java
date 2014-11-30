b26a5e600ad1f6d190b956b980f53ed9202192ec
AoschkA <jonaspraem@gmail.com>
2014-11-30 15:43:31 +0100

    fjernet fejlrapport

--- CDIO3/src/control/Launch.java
+++ CDIO3/src/control/Launch.java
@@ -6,14 +6,6 @@ public class Launch {
 	public static void main(String[] args) {
 		GameController control = new GameController();
 		control.init();
-		/** 
-		 * fejl:
-		 * TAX HAR GUI KONTAKT
-		 * det er en løsning, hilsen seb
-		 * labor camps duer ik.... ingen udveksling af penge nå man lander på den
-		 * 
-		 */
-
 	}
 
 }
